f00187cb647fb03340656a2a22a3ea6bfd1fb5f8
David Vercruyssen <david@aranea.be>
2021-11-18 11:56:43 +0100

    Upd to dotnet6

--- Responsers/HelloResponder.cs
+++ Responsers/HelloResponder.cs
@@ -2,24 +2,23 @@
 using System.Text;
 using MargieBot;
 
-namespace Kerstbot.Responsers
+namespace Kerstbot.Responsers;
+
+public class HelloResponder : IResponder
 {
-    public class HelloResponder : IResponder
+    public bool CanRespond(ResponseContext context)
     {
-        public bool CanRespond(ResponseContext context)
-        {
-            return !context.BotHasResponded
-                   && !context.Message.User.IsSlackbot
-                   && (context.Message.MentionsBot || context.Message.ChatHub.Type == SlackChatHubType.DM)
-                   && context.Message.Text.ToLower().Contains("hello");
-        }
+        return !context.BotHasResponded
+               && !context.Message.User.IsSlackbot
+               && (context.Message.MentionsBot || context.Message.ChatHub.Type == SlackChatHubType.DM)
+               && context.Message.Text.ToLower().Contains("hello");
+    }
 
-        public BotMessage GetResponse(ResponseContext context)
-        {
-            var builder = new StringBuilder();
-            builder.Append("Hello ").Append(context.Message.User.FormattedUserID);
+    public BotMessage GetResponse(ResponseContext context)
+    {
+        var builder = new StringBuilder();
+        builder.Append("Hello ").Append(context.Message.User.FormattedUserID);
 
-            return new BotMessage { Text = builder.ToString() };
-        }
+        return new BotMessage { Text = builder.ToString() };
     }
 }
--- Responsers/ZoomResponder.cs
+++ Responsers/ZoomResponder.cs
@@ -1,35 +1,34 @@
 ﻿using MargieBot;
 using System.Text.RegularExpressions;
 
-namespace Kerstbot.Responsers
+namespace Kerstbot.Responsers;
+
+public class ZoomResponder : IResponder
 {
-    public class ZoomResponder : IResponder
+    public bool CanRespond(ResponseContext context)
     {
-        public bool CanRespond(ResponseContext context)
-        {
-            var respond = !context.BotHasResponded
-                && !context.Message.User.IsSlackbot
-                && (context.Message.MentionsBot || context.Message.ChatHub.Type == SlackChatHubType.DM)
-                && context.Message.Text.ToLower().Contains("zoom.us");
+        var respond = !context.BotHasResponded
+            && !context.Message.User.IsSlackbot
+            && (context.Message.MentionsBot || context.Message.ChatHub.Type == SlackChatHubType.DM)
+            && context.Message.Text.ToLower().Contains("zoom.us");
 
-            return respond;
+        return respond;
+    }
+
+    public BotMessage GetResponse(ResponseContext context)
+    {
+        var messageText = context.Message.Text;
+        var pattern = @"<https?:\/\/([^\|\>]*)(\|[^>]*)?>";
+        var m = Regex.Match(messageText, pattern);
+
+        if (m.Success)
+        {
+            var url = "https://" + m.Groups[1].Value;
+            Utils.OpenUrl(url);
+
+            return new BotMessage { Text = "Ho, ho, ho! It's time to get jolly on your naughty asses! (" + url + ")" };
         }
 
-        public BotMessage GetResponse(ResponseContext context)
-        {
-            var messageText = context.Message.Text;
-            var pattern = @"<https?:\/\/([^\|\>]*)(\|[^>]*)?>";
-            var m = Regex.Match(messageText, pattern);
-
-            if (m.Success)
-            {
-                var url = "https://" + m.Groups[1].Value;
-                Utils.OpenUrl(url);
-
-                return new BotMessage { Text = "Ho, ho, ho! It's time to get jolly on your naughty asses! (" + url + ")" };
-            }
-
-            return new BotMessage { Text = "Sorry, I could not locate a meeting URL in your message..." };
-        }
+        return new BotMessage { Text = "Sorry, I could not locate a meeting URL in your message..." };
     }
 }
